fcfa799500f9f58dc4a814972e7ab8e29116f150
Ruiz, Héctor <hectoruiz.89@gmail.com>
2020-05-06 01:47:58 +0200

    Add navigation jetpack library dependency. Set up the graph of the navigation of the app.

--- feature-searcher/src/main/java/com/hectoruiz/feature/searcher/presentation/searcher/ui/ListFragment.kt
+++ feature-searcher/src/main/java/com/hectoruiz/feature/searcher/presentation/searcher/ui/ListFragment.kt
@@ -1,18 +1,12 @@
 package com.hectoruiz.feature.searcher.presentation.searcher.ui
 
-import android.os.Bundle
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.ViewModelProviders
 import com.hectoruiz.feature.searcher.R
 import com.hectoruiz.feature.searcher.presentation.searcher.viewmodel.ListViewModel
 
-class ListFragment : Fragment(R.layout.search_fragment) {
+class ListFragment : Fragment(R.layout.list_fragment) {
 
     private val viewModel: ListViewModel by viewModels()
 
-    companion object {
-        fun newInstance() =
-            ListFragment()
-    }
 }
